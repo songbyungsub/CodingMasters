449367bda639851c61687ba14115e53d521847a5
byung-sub <98787306+songbyungsub@users.noreply.github.com>
2022-09-13 10:23:19 +0900

    Add files via upload

---
+++ 3259.py
@@ -0,0 +1,12 @@
+n = int(input())
+if n ==1:
+    print(8)
+elif n == 2: print(98)
+elif n == 3: print(998)
+elif n == 4: print(9998)
+elif n == 5: print(99998)
+elif n == 6: print(999998)
+elif n == 7: print(9999998)
+elif n == 8: print(99999998)
+else:
+    print(999999998)
