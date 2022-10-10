f30ab36c1a48502803e563597e1acf8922eadbfc
byung-sub <98787306+songbyungsub@users.noreply.github.com>
2022-10-10 23:19:11 +0900

    Add files via upload

---
+++ 3301.py
@@ -0,0 +1,13 @@
+a,b,c,d=map(str,input().split())
+if int(a)==0:
+    print(0)
+else:
+    if d=='Private':
+        if int(a)==5 or int(a)==6:
+            print(20)
+        elif c=='N' and (b=='ROKA' or b =='ROKN'):
+            print(32)
+        else:
+            print(28)
+    else:
+        print(28)
